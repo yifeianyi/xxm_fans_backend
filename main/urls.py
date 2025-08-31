6f00be020b9ba285dee209343e807144276a0801
yifeianyi <910002662@qq.com>
2025-08-31 21:39:19 +0800

    更新配置信息

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ from .views import song_list_api, top_songs_api, is_mobile_api
 urlpatterns = [
     # path('',views.index, name="index")
     path("",views.songs_list,name="歌单"),
+#    path("",admin.site.urls)
     path('api/songs/<int:song_id>/records',views.song_records_api,name= "song_records_api"),
     path("api/songs",song_list_api),
     path('api/styles', views.style_list_api),
